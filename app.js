2682470b8ac6b6b5d9a7ea5452c1f7d816dc4f53
Destoky <adestoky@gmail.com>
2020-11-26 18:32:39 +0100

    Switch de cadena et couleur complementaire

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //SELECTION DU DOM
 let couleurs = document.querySelectorAll(".couleur");
 let btnsCopier = document.querySelectorAll(".fa-copy");
+let btnsCadena = document.querySelectorAll(".fa-lock-open");
+let btns = document.querySelectorAll(".fa");
 
 //VARIABLES DE TRAVAIL
 
+//-------------FONCTIONS -------------------------------------------------
+
+//RETOURNE UNE COULEUR ALEATOIRE
 let randomCouleurs = () => {
   let redRandom = Math.floor(Math.random() * 256);
   let greenRandom = Math.floor(Math.random() * 256);
@@ -11,14 +16,33 @@ let randomCouleurs = () => {
   return `rgb(${redRandom},${greenRandom},${blueRandom})`;
 };
 
+//DONNE UN COULEUR A CHAQUE DIV DE COULEURS
 let attributionCouleurs = () => {
   for (const couleur of couleurs) {
-    couleur.style.backgroundColor = randomCouleurs();
+    let couleurDiv = randomCouleurs();
+    couleur.style.backgroundColor = couleurDiv;
+    let boutonDansCouleur = couleur.children;
+    console.log(boutonDansCouleur);
+    //DONNE A CHAQUE BTN dans de le div couleur une couleur opposé
+    for (const btn of boutonDansCouleur) {
+      btn.style.color = couleurOppose(couleurDiv);
+    }
   }
+};
+
+let couleurOppose = (rgb) => {
+  let colors = rgb.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+  let red = colors[1];
+  let green = colors[2];
+  let blue = colors[3];
+  return `rgb(${255 - red},${255 - green},${255 - blue})`;
 };
 
 attributionCouleurs();
 
+//-----------------ECOUTEUR D'EVENEMENTS---------------------------
+
+//Change les couleurs quand on appuie sur espace
 document.body.addEventListener("keyup", (e) => {
   if (e.code == "Space") {
     attributionCouleurs();
@@ -38,9 +62,24 @@ for (const btnCopier of btnsCopier) {
     let notifCopie = btnCopier.firstElementChild;
     notifCopie.innerHTML = "RGB Copié";
     notifCopie.visibility = "visible";
-    setTimeout(()=> {
+    setTimeout(() => {
       notifCopie.innerHTML = "Copier le code RGB";
       notifCopie.visibility = "hidden";
-    },1000); 
+    }, 1000);
   });
 }
+
+//SWITCH de cadena au click
+for (const btn of btnsCadena) {
+  console.log(btn);
+    btn.addEventListener("click",()=> {
+      btn.classList.toggle("fa-lock");
+      btn.classList.toggle("fa-lock-open");
+      let notifVerrou = btn.firstElementChild;
+      if(notifVerrou.textContent == "Verrouillez la couleur") {
+        notifVerrou.textContent = "Deverrouillez la couleur";
+      } else {
+        notifVerrou.textContent = "Verrouillez la couleur";
+      }
+    })
+  }
