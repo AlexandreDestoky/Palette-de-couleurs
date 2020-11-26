d7b900d307be3a1075bf14b98adeef3e2446857e
Destoky <adestoky@gmail.com>
2020-11-26 16:22:23 +0100

    ajouter icones fontawesome

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
- //SELECTION DU DOM
- let couleurs = document.querySelectorAll(".couleur");
-
+//SELECTION DU DOM
+let couleurs = document.querySelectorAll(".couleur");
 
 //VARIABLES DE TRAVAIL
 
 let randomCouleurs = () => {
-  let redRandom = Math.floor(Math.random()* 256);
-  let greenRandom = Math.floor(Math.random()* 256);
-  let blueRandom = Math.floor(Math.random()* 256);
+  let redRandom = Math.floor(Math.random() * 256);
+  let greenRandom = Math.floor(Math.random() * 256);
+  let blueRandom = Math.floor(Math.random() * 256);
   return `rgb(${redRandom},${greenRandom},${blueRandom})`;
-}
+};
 
 let attributionCouleurs = () => {
   for (const couleur of couleurs) {
     couleur.style.backgroundColor = randomCouleurs();
-  }  
-}
+  }
+};
 
 attributionCouleurs();
 
-
-
-
-document.body.addEventListener("keyup",(e)=> {
-  if(e.code == "Space") {
+document.body.addEventListener("keyup", (e) => {
+  if (e.code == "Space") {
     attributionCouleurs();
   }
-})
-
-
-
+});
